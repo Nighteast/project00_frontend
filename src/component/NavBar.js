345ad74d64b00b63a6b3c55aa4d4471bb9a8e332
Nighteast <jung946244@naver.com>
2023-11-21 22:56:54 +0900

    NavBar 기본 항목 추가

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -5,9 +5,13 @@ export function NavBar() {
   const navigate = useNavigate();
 
   return (
-    <Flex>
-      <Button onClick={() => navigate("/")}>home</Button>
-      <Button onClick={() => navigate("/write")}>write</Button>
+    <Flex m={"10px"} gap={"5px"}>
+      <Button onClick={() => navigate("/")}>@home</Button>
+      <Button onClick={() => navigate("/write")}>@write</Button>
+      <Button onClick={() => navigate("/login")}>@login</Button>
+      <Button onClick={() => navigate("/logout")}>@logout</Button>
+      <Button onClick={() => navigate("/profile")}>@profile</Button>
+      <Button onClick={() => navigate("/memberList")}>@memberList</Button>
     </Flex>
   );
 }
